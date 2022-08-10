648522f5595358e0f09a8746c814e3d5c0a9cd3f
Bernardo Busatto <berbusatto@gmail.com>
2022-08-09 22:17:17 -0300

    Adicionando primeiros helpers

--- bootstrap/Env.php
+++ bootstrap/Env.php
@@ -1,7 +1,31 @@
 <?php
 class Env{
 
-    public function __construct() {
+    // private $mainDir;
+    
+    // public function __construct() {
+    //     $this->mainDir = __DIR__;
+        
+    // }
+
+    public static function execute(){
+        $contentOfEnvFile = file_get_contents( __DIR__ . "\..\.env");
+
+        $arrayEnv = explode("\n", $contentOfEnvFile);
+
+        foreach($arrayEnv as $value){
+            $keyAndValue = explode("=", $value);
+
+            if(!isset($keyAndValue[1])){
+                continue; // pare aqui neste ciclo se o valor nao existir
+            }
+            $nameOfVariable = $keyAndValue[0];
+            $valueOfVariable = $keyAndValue[1];
+
+            $_ENV[$nameOfVariable] = $valueOfVariable;
+
+
+        }
         
     }
 
