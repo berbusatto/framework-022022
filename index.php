441ebb49758412d598bb9c8ace94c56713e57fca
Bernardo Busatto <berbusatto@gmail.com>
2022-08-13 09:44:37 -0300

    inserindo abstract factory

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $mainPosition = __DIR__;
 
 
 require_once("{$mainPosition}\helper\helper.php"); // helper sempre serão usados, por isso são requireds
-require_once("{$mainPosition}\\vendor\autoload.php"); // é a o arquivo que guarda as configurações do use
+require_once("{$mainPosition}\\vendor\autoload.php"); // é a o arquivo que guarda as configurações dos use
 
 use Bootstrap\Env; // alteramos o require_once para o use Bootstrap
 use App\frameworkTools\ProcessServerElements;
