370359675214dcbb1475e3c604294af1e9e81fe7
Bernardo Busatto <berbusatto@gmail.com>
2022-09-08 09:51:00 -0300

    criando nova rota

--- app/frameworkTools/Implementations/route/RouteProcess.php
+++ app/frameworkTools/Implementations/route/RouteProcess.php
@@ -4,6 +4,7 @@ namespace App\FrameworkTools\Implementations\Route;
 
 use App\FrameworkTools\ProcessServerElements;
 use App\Controllers\HelloWorldController;
+use App\Controllers\TrainingQueryController;
 
 class RouteProcess{
     public static function execute(){
@@ -17,9 +18,12 @@ class RouteProcess{
                     case '/hello-world':
                         return (new HelloWorldController)->execute();
                     break;
+                    case '/training-query':
+                        return (new TrainingQueryController)->execute();
+                    break;
                 }
-            case 'POST':
-                return (new DataInputController)-> execute();
+            // case 'POST':
+            //     return (new DataInputController)-> execute();
 
         }
        
