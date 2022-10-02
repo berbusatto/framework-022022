4fd17735ff08fbce86742872c45977df34b453ea
Bernardo Busatto <berbusatto@gmail.com>
2022-10-02 11:58:47 -0300

    alteracoes no abstract controller

--- app/Controllers/PostLessonController.php
+++ app/Controllers/PostLessonController.php
@@ -42,10 +42,36 @@ class PostLessonController extends AbstractControllers {
             throw new \Exception('ERROR: Missing NAME on request.');
         }
 
-        if(!$this->params['name']){
-            $this->attrName = 'name';
-            throw new \Exception('ERROR: Missing NAME on request.');
+        if(!$this->params['car_model']){
+            $this->attrName = 'car_model';
+            throw new \Exception('ERROR: Missing CAR MODEL on request.');
         }
+
+        if(!$this->params['year']){
+            $this->attrName = 'year';
+            throw new \Exception('ERROR: Missing YEAR on request.');
+        }
+
+        if(!$this->params['motorization']){
+            $this->attrName = 'motorization';
+            throw new \Exception('ERROR: Missing MOTORIZATION on request.');
+        }
+        
+        if($this->params['year'] < 1900){
+            $this->attrName = 'year';
+            throw new \Exception('ERROR: YEAR must be greater than 1900.');
+        }
+
+
+        // TENTEI 
+        if($this->params['year'] > getdate(['year'])){
+            $this->attrName = 'year';
+            throw new \Exception('ERROR: YEAR must be minor or equal actual year.');
+        }
+
+        
+        
+        
 
 
     }
