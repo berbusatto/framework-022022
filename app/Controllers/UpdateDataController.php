d6a1cea44a313d86c3a1ea77d82565dc631bd0d7
Bernardo Busatto <berbusatto@gmail.com>
2022-10-19 16:31:32 -0300

    corrige controller

--- app/Controllers/UpdateDataController.php
+++ app/Controllers/UpdateDataController.php
@@ -13,12 +13,11 @@ class UpdateDataController extends AbstractControllers{
             'success' => true
         ];
         try{
-            
             $requestVariables =  $this->processServerElements->getVariables();
 
             if ((!$requestVariables) || (sizeof($requestVariables) === 0)){
-                $missingAttribute = 'car_id is null';
-                throw (new \Exception("Insert variable in URL"));
+                $missingAttribute = 'missingid_carinurl';
+                throw (new \Exception("Insert variable in URL."));
             }
 
             foreach ($requestVariables as $requestVariable){
@@ -27,22 +26,83 @@ class UpdateDataController extends AbstractControllers{
                 }
             }
 
+           
+
             if(!$carId){
-                $missingAttribute = 'id_car is empty';
-                throw (new \Exception("car_id is not valid"));
+                $missingAttribute = 'id_carnotvalid';
+                throw (new \Exception("id_car is not valid."));
             }
 
             $cars = $this->pdo->query("SELECT * FROM car WHERE id_car = '{$carId}';")
                 ->fetchAll();
 
+
             if(sizeof($cars) === 0){
-                $missingAttribute = 'this car does not exist';
+                $missingAttribute = 'thiscardoesnotexist';
                 throw new \Exception("Record did not found.");
             }
 
             $params = $this->processServerElements->getInputJSONData();
-            dd($params);
-                       
+            
+
+            if ((!$params) || sizeof($params) === 0){
+                $missingAttribute = 'paramsdoesnotexists';
+                throw new \Exception("You need to inform the params attribute.");
+
+            }
+
+             //MANIPULANDO A STRING QUE SERA O QUERY DO SQL POSTERIORMENTE
+            $updateStructureQuery = '';
+
+            foreach ($params as $key => $value){
+                if (!in_array($key,['name','car_model', 'year', 'motorization'])){
+                    $missingAttribute = 'keynotacceptable';
+                    throw new \Exception($key);
+                }
+
+                if ($key === 'name'){
+                    $updateStructureQuery .= "name = :name,";
+                }
+
+                if ($key === 'car_model'){
+                    $updateStructureQuery .= "car_model = :car_model,";
+                }
+
+                if ($key === 'year'){
+                    $updateStructureQuery .= "year = :year,";
+                }
+
+                if ($key === 'motorization'){
+                    $updateStructureQuery .= "motorization = :motorization,";
+                }
+            }
+
+            // PARA REMOVER A VÍRGULA DO FINAL DA STRING
+            // CONVERTE-SE STRING EM ARRAY
+            $updatedStringInArray = str_split($updateStructureQuery);
+
+            // DARIA PRA RESOLVER SÓ COM O SUBSTR (EU ACHO)
+            // dd(substr($updateStructureQuery,0,-1));
+
+            //REMOVE O ÚLTIMO ELEMENTO
+            array_pop($updatedStringInArray);
+
+            // CONVERTE O ARRAY PARA STRING
+            $newStringElementsSQL = implode($updatedStringInArray);
+            
+            // dd($updatedStringInArray);
+
+            $sql = "UPDATE car SET {$newStringElementsSQL} WHERE id_car = {$carId}";            
+
+            $statement = $this->pdo->prepare($sql);
+            // dd($statement);
+            
+            $statement->execute([
+                ':name' => $params["name"],
+                ':car_model' => $params["car_model"],
+                ':year' => $params["year"],
+                ':motorization' => $params["motorization"]                
+            ]);
 
         } catch(\Exception $e){
             $response = [
