109fec5ff6fca3b3a0a8598b0a28d6131609d7b3
Bernardo Busatto <berbusatto@gmail.com>
2022-11-17 18:49:52 -0300

    projeto

---
+++ app/frameworkTools/Implementations/route/Delete.php
@@ -0,0 +1,16 @@
+<?php 
+namespace App\FrameworkTools\Implementations\Route;
+
+use App\Controllers\UpdateDataController;
+
+trait Delete {
+    private static function put(){
+        switch(self::$processServerElements->getRoute()){
+            case '':
+                return (new )->execute();
+            break;
+            case '':
+                return (new )->busatto000();
+        }
+    }
+}
