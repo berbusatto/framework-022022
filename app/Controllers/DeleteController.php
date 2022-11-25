13aea5ae1a508d1c5f8641e4f6d2985295e4e296
Bernardo Busatto <berbusatto@gmail.com>
2022-11-24 22:53:46 -0300

    delete controller

--- app/Controllers/DeleteController.php
+++ app/Controllers/DeleteController.php
@@ -8,13 +8,44 @@ class DeleteController extends AbstractControllers{
 
     public function execute(){
         $requestsVariables = $this->processServerElements->getVariables();
+        $response = ['sucess' => true];
         $idUser;
+        $missingAttribute;
 
-        foreach($requestsVariables as $valueVariable){
-            if($valueVariable['name'] === "id_user"){
-                $idUser = $valueVariable['value'];
+        try{
+            foreach($requestsVariables as $valueVariable){
+                if($valueVariable['name'] === 'id_user'){
+                    $idUser = $valueVariable['value'];
+                }
             }
+            
+            if(!$idUser){
+                $missingAttribute = 'userIdNull';
+                throw new \Exception("You need to inform userId");
+            }
+
+            $users = $this->pdo->query("SELECT * FROM user WHERE id_user ='{$idUser}';")->fetchAll();
+
+            if(sizeof($users) === 0){
+                $missingAttribute = 'userDoesntExists';
+                throw new \Exception("No record of this user");
+            }
+
+            $sql = 'DELETE FROM user WHERE id_user = :id_user';
+    
+            $statement = $this->pdo->prepare($sql);
+            $statement->execute(['id_user'=> $idUser]);
+          
+
+        } catch(\Exception $e){
+            $response = [
+                'success' => false,
+                'message' => $e->getMessage(),
+                'missingAttribute' => $missingAttribute
+            ];
         }
-        dd($idUser);
+
+          view($response);
+        
     }
 }
